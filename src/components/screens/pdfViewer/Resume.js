c9536680d4ed2e7ca70e66176f9d41724c088303
unknown <jaskin.agmata@gmail.com>
2020-09-23 08:19:25 -0700

    adding Preloader to the pdf section

--- src/components/screens/pdfViewer/Resume.js
+++ src/components/screens/pdfViewer/Resume.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Lines } from "react-preloaders";
 
-//import PdfViewer component
+//import components
 import Resume from "../../parts/pdfViewer/PDFViewer";
+import PreLoaders from "../../parts/PreLoader/PreLoader";
 
 //import Css for pdf format
 import "./Resume.css";
@@ -10,12 +12,15 @@ import "./Resume.css";
 import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const PDFViewer = () => {
-  return (
-    <div className="all-page-container">
-      <Resume pdf={"assets/pdf/RESUME.pdf"} />
-    </div>
-  );
-};
-
-export default PDFViewer;
+export default class PDFViewer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="all-page-container">
+          <Resume pdf={"assets/pdf/RESUME.pdf"} />
+        </div>
+        <Lines />
+      </React.Fragment>
+    );
+  }
+}
